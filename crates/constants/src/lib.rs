4249d09a4e3beae9e9a2dd8fdcd6f61ee6ace350
lilixac <hinewton25@gmail.com>
2025-04-16 09:59:15 +0545

    added constants

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -1,2 +1,5 @@
+pub mod chains;
+pub mod eth_contracts;
 pub mod precompiles;
 pub mod sequencer;
+pub mod twine;
---
+++ crates/constants/src/twine.rs
@@ -0,0 +1,5 @@
+use reth::revm::primitives::{address, Address};
+
+/// Twine System Storage Contract
+pub const TwineSystemStorageContract: Address =
+    address!("f9cCb674b4455F7c49Cd119Fde6c2208f0eD4976");
