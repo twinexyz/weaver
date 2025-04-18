72cfdc5f1dfdfb99b7d40cc65809a5918f4c505e
lilixac <hinewton25@gmail.com>
2025-04-18 11:29:51 +0545

    chore: rename res -> artifacts

--- crates/evm-contracts/src/lib.rs
+++ crates/evm-contracts/src/lib.rs
@@ -6,19 +6,19 @@ sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
     TwineChain,
-    "res/TwineChain.json"
+    "artifacts/TwineChain.json"
 );
 
 sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
     L2TwineMessenger,
-    "res/L2TwineMessenger.json"
+    "artifacts/L2TwineMessenger.json"
 );
 
 sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
     L1MessageQueue,
-    "res/L1MessageQueue.json"
+    "artifacts/L1MessageQueue.json"
 );
