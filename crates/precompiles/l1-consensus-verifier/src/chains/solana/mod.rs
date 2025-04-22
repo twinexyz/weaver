33268abb91c441330dc4d950bb43adf89e0898bf
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-04-22 08:35:33 +0545

    feat: initial commit for verifier precompile

---
+++ crates/precompiles/l1-consensus-verifier/src/chains/solana/mod.rs
@@ -0,0 +1,11 @@
+use crate::Chains;
+
+#[derive(Debug, Clone)]
+pub struct SolanaConsensusVerifier {}
+
+impl Chains for SolanaConsensusVerifier {
+    fn verify(&self, input: &alloy_primitives::Bytes) -> reth::revm::primitives::PrecompileResult {
+        _ = input;
+        todo!();
+    }
+}
