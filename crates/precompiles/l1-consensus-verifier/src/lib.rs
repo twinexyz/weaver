d58bade9d52a1a9c9cd5aff4b2a8add3e33bab55
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-04-22 08:35:33 +0545

    feat: initial commit for verifier precompile

--- crates/precompiles/l1-consensus-verifier/src/lib.rs
+++ crates/precompiles/l1-consensus-verifier/src/lib.rs
@@ -1,31 +1,100 @@
+pub mod chains;
+
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::sync::Arc;
 
 use alloy_primitives::Bytes;
-use reth::revm::primitives::{Precompile, PrecompileOutput, PrecompileResult, StatefulPrecompile};
+use alloy_sol_types::{sol_data, SolType};
+use chains::ethereum::SolanaConsensusVerifier;
+use reth::revm::primitives::{
+    Precompile, PrecompileError, PrecompileErrors, PrecompileResult, StatefulPrecompile,
+};
 use reth::revm::ContextPrecompile;
+use twine_constants::chains::{
+    ETHEREUM_CHAIN_ID, ETHEREUM_HOLESKY, ETHEREUM_HOLESKY_CHAIN_ID, ETHEREUM_MAINNET,
+    ETHEREUM_SEPOLIA, ETHEREUM_SEPOLIA_CHAIN_ID, SOLANA_CHAIN_ID, SOLANA_DEVNET,
+    SOLANA_DEVNET_CHAIN_ID, SOLANA_MAINNET,
+};
 
-#[derive(Clone)]
-pub struct ConsensusVerifierPrecompile {}
+#[derive(Debug)]
+pub struct ConsensusVerifierPrecompile {
+    pub chains: HashMap<String, Box<dyn Chains>>,
+}
+
+pub struct Test {}
+
+pub trait Chains: Debug + Send + Sync {
+    fn verify(&self, input: &Bytes) -> PrecompileResult;
+}
 
 impl ConsensusVerifierPrecompile {
     pub fn new_ordinary<DB>() -> ContextPrecompile<DB>
     where
         DB: reth_evm::Database, {
-        ContextPrecompile::Ordinary(Precompile::Stateful(Arc::new(Self {})))
+        let mut chains: HashMap<String, Box<dyn Chains>> = HashMap::new();
+        // register solana devnet
+        chains.insert(
+            String::from(SOLANA_DEVNET),
+            Box::new(SolanaConsensusVerifier {}),
+        );
+        ContextPrecompile::Ordinary(Precompile::Stateful(Arc::new(Self { chains })))
     }
 }
+
+pub type VerifierInput = (sol_data::Uint<64>, sol_data::Bytes);
 
 impl StatefulPrecompile for ConsensusVerifierPrecompile {
     fn call(
         &self,
-        _bytes: &alloy_primitives::Bytes,
-        _gas_limit: u64,
-        _env: &reth::revm::primitives::Env,
+        bytes: &Bytes,
+        gas_limit: u64,
+        env: &reth::revm::primitives::Env,
     ) -> PrecompileResult {
-        println!("+++++++++ PRECOMPILE CALLED +++++++++");
-        Ok(PrecompileOutput {
-            gas_used: 0,
-            bytes: Bytes::from_static(&[9, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
-        })
+        _ = gas_limit;
+        _ = env;
+        match VerifierInput::abi_decode_sequence(&bytes, true) {
+            Ok((chain_id, precompile_input)) => match chain_id {
+                ETHEREUM_CHAIN_ID => {
+                    if let Some(eth_mainnet_verifier) = self.chains.get(ETHEREUM_MAINNET) {
+                        return eth_mainnet_verifier.verify(&precompile_input);
+                    }
+                }
+                ETHEREUM_HOLESKY_CHAIN_ID => {
+                    if let Some(eth_mainnet_verifier) = self.chains.get(ETHEREUM_HOLESKY) {
+                        return eth_mainnet_verifier.verify(&precompile_input);
+                    }
+                }
+
+                ETHEREUM_SEPOLIA_CHAIN_ID => {
+                    if let Some(eth_mainnet_verifier) = self.chains.get(ETHEREUM_SEPOLIA) {
+                        return eth_mainnet_verifier.verify(&precompile_input);
+                    }
+                }
+
+                SOLANA_CHAIN_ID => {
+                    if let Some(eth_mainnet_verifier) = self.chains.get(SOLANA_MAINNET) {
+                        return eth_mainnet_verifier.verify(&precompile_input);
+                    }
+                }
+
+                SOLANA_DEVNET_CHAIN_ID => {
+                    if let Some(eth_mainnet_verifier) = self.chains.get(SOLANA_DEVNET) {
+                        return eth_mainnet_verifier.verify(&precompile_input);
+                    }
+                }
+                _ =>
+                    return PrecompileResult::Err(PrecompileErrors::Error(PrecompileError::Other(
+                        String::from("not implemented"),
+                    ))),
+            },
+            Err(e) =>
+                return PrecompileResult::Err(PrecompileErrors::Error(PrecompileError::Other(
+                    e.to_string(),
+                ))),
+        }
+        PrecompileResult::Err(PrecompileErrors::Error(PrecompileError::Other(
+            String::from("not implemented"),
+        )))
     }
 }
