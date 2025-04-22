a72971d830ffd45461b8c044dfd1bd0fa4a8819c
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-04-22 08:34:52 +0545

    feat: added chains constants

--- crates/constants/src/chains.rs
+++ crates/constants/src/chains.rs
@@ -1,7 +1,14 @@
 // Ethereum
+pub const ETHEREUM_MAINNET: &str = "ethereum_mainnet";
+pub const ETHEREUM_HOLESKY: &str = "ethereum_holesky";
+pub const ETHEREUM_SEPOLIA: &str = "ethereum_sepolia";
+
 pub const ETHEREUM_CHAIN_ID: u64 = 1;
 pub const ETHEREUM_HOLESKY_CHAIN_ID: u64 = 17000;
 pub const ETHEREUM_SEPOLIA_CHAIN_ID: u64 = 11155111;
 
 // Solana
+pub const SOLANA_MAINNET: &str = "solana_mainnet";
+pub const SOLANA_DEVNET: &str = "solana_devnet";
 pub const SOLANA_CHAIN_ID: u64 = 900;
+pub const SOLANA_DEVNET_CHAIN_ID: u64 = 901;
