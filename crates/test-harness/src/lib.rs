6cb7d8164ad7e2eda9a47476e2298f4ae6ccb340
lilixac <hinewton25@gmail.com>
2025-04-22 17:51:00 +0545

    review fixes

--- crates/test-harness/src/lib.rs
+++ crates/test-harness/src/lib.rs
@@ -19,6 +19,34 @@ use log::{error, info};
 /// This is used to pass params between services or async functions
 pub type ContextArena = Rc<RefCell<HashMap<String, String>>>;
 
+/// A parser function type that takes a string slice (typically a line of
+/// output) and returns an optional processed `String`.
+///
+/// This is typically used to parse `stdout` or `stderr` lines emitted by a
+/// subprocess.
+///
+/// The `Parser` is wrapped in an `Arc` to allow shared ownership across
+/// threads, and it must be both `Send` and `Sync` to be safely used in
+/// concurrent contexts.
+pub type Parser = Arc<dyn Fn(&str) -> Option<String> + Send + Sync>;
+
+/// Represents an optional stream parser for either `stdout` or `stderr`.
+///
+/// If present, the tuple contains:
+/// - An `mpsc::Sender<String>`: Used to send parsed log lines to another task
+///   or consumer.
+/// - A `Parser`: A function that processes lines of text and optionally
+///   filters/transforms them.
+///
+/// This type is used to hook into a subprocess's output stream and selectively
+/// process and forward its logs.
+///
+/// # Usage
+/// This type is passed to a service (e.g. `add_stdout_parser(StreamParser)`) to
+/// attach a parser to an output stream. If `None`, no parsing or forwarding
+/// will be done for that stream.
+type StreamParser = Option<(mpsc::Sender<String>, Parser)>;
+
 /// A single step of a test
 #[derive(Debug)]
 pub enum TestStep {
@@ -81,8 +109,41 @@ impl TestHarness {
     }
 
     /// Adds a service to the test harness.
-    pub fn add_service(&mut self, service: Box<dyn Service<ServiceError = String>>) {
+    /// ## Returns:
+    /// A tuple of two `Option<mpsc::Receiver<String>>`:
+    /// - `stdout_rx`: An `Option` containing a receiver for the `stdout` logs
+    ///   if a `stdout_parser` is provided. If no parser is given, this will be
+    ///   `None`.
+    /// - `stderr_rx`: An `Option` containing a receiver for the `stderr` logs
+    ///   if a `stderr_parser` is provided. If no parser is given, this will be
+    ///   `None`.
+    pub fn add_service(
+        &mut self,
+        mut service: Box<dyn Service<ServiceError = String>>,
+        stdout_parser: Option<Parser>,
+        stderr_parser: Option<Parser>,
+    ) -> (
+        Option<mpsc::Receiver<String>>,
+        Option<mpsc::Receiver<String>>,
+    ) {
+        let mut stdout_rx = None;
+        let mut stderr_rx = None;
+
+        if let Some(out_parser) = stdout_parser {
+            let (stdout_tx, rx) = mpsc::channel();
+            service.add_stdout_parser(Some((stdout_tx, out_parser)));
+            stdout_rx = Some(rx);
+        }
+
+        if let Some(err_parser) = stderr_parser {
+            let (stderr_tx, rx) = mpsc::channel();
+            service.add_stderr_parser(Some((stderr_tx, err_parser)));
+            stderr_rx = Some(rx);
+        }
+
         self.services.push(service);
+
+        (stdout_rx, stderr_rx)
     }
 
     /// Adds a test step to be executed later.
@@ -181,10 +242,8 @@ impl ServiceStepExecutor for SubProcessServiceStarter {
             return Err(format!("Service '{}' is already running", self.name));
         }
 
-        let log_relay = service.get_log_line_relay();
-
         service
-            .start(ctx, log_relay)
+            .start(ctx)
             .map_err(|e| format!("Failed to start service '{}': {}", self.name, e))?;
         if let Some(wait_duration) = self.wait_after {
             std::thread::sleep(wait_duration);
@@ -251,11 +310,7 @@ pub trait Service: Debug {
     type ServiceError;
 
     /// Starts the service using the shared context.
-    fn start(
-        &mut self,
-        ctx: ContextArena,
-        log_util: LogLineRelay,
-    ) -> Result<(), Self::ServiceError>;
+    fn start(&mut self, ctx: ContextArena) -> Result<(), Self::ServiceError>;
 
     /// Returns true if the service is currently running.
     fn is_running(&self) -> bool;
@@ -263,14 +318,19 @@ pub trait Service: Debug {
     /// Stops the service gracefully.
     fn stop(&mut self) -> Result<(), Self::ServiceError>;
 
-    /// Get LogLineRelay
-    fn get_log_line_relay(&self) -> LogLineRelay;
+    /// Get stdout parser
+    fn add_stdout_parser(&mut self, stdout: StreamParser);
+
+    /// Get stderr parser
+    fn add_stderr_parser(&mut self, stderr: StreamParser);
 }
 
 /// Running service
 pub struct SubProcessService {
     /// Service Identifier
     pub name: String,
+    /// Service Description
+    pub description: String,
     /// The first element should be the command
     /// Others should be args
     pub cmd_gen: Box<dyn Fn(ContextArena) -> Vec<String>>,
@@ -278,43 +338,17 @@ pub struct SubProcessService {
     pub child: Option<Child>,
     /// Mutate the context arena with parsed output
     pub context_arena: Option<ContextArena>,
-
-    /// stdout and stderr utils
-    pub log_relay: LogLineRelay,
-}
-
-/// If `stdout_sender` is null, `stdout_parser` should be null
-/// If `stdout_sender` is not null,
-///     `stdout_parser` is null -> entire lines parsed on stdout sent through
-/// channel     `stdout_parser` is not null -> line after parsing is send
-/// through channel Same logic applies for stderr too
-pub struct LogLineRelay {
-    /// Stdout output parser
-    pub stdout_parser: Option<Arc<dyn Fn(&str) -> Option<String> + Send + Sync>>,
-    /// Stderr output parser
-    pub stderr_parser: Option<Arc<dyn Fn(&str) -> Option<String> + Send + Sync>>,
-
-    /// Stdout log sender
-    pub stdout_sender: Option<mpsc::Sender<String>>,
-    /// Stderr log sender
-    pub stderr_sender: Option<mpsc::Sender<String>>,
-}
-
-impl Debug for LogLineRelay {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("LogUtil")
-            .field("stdout_parser", &self.stdout_parser.is_some())
-            .field("stderr_parser", &self.stderr_parser.is_some())
-            .field("stdout_sender", &self.stdout_sender)
-            .field("stderr_sender", &self.stderr_sender)
-            .finish()
-    }
+    /// stdout
+    pub stdout_stream: StreamParser,
+    /// stderr
+    pub stderr_stream: StreamParser,
 }
 
 impl Debug for SubProcessService {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SubProcessService")
             .field("name", &self.name)
+            .field("description", &self.description)
             .finish()
     }
 }
@@ -322,30 +356,41 @@ impl Debug for SubProcessService {
 impl Service for SubProcessService {
     type ServiceError = String;
 
-    fn start(&mut self, ctx: ContextArena, log_util: LogLineRelay) -> Result<(), String> {
+    fn start(&mut self, ctx: ContextArena) -> Result<(), String> {
         if self.is_running() {
             return Err(format!("Subprocess '{}' is already running", self.name));
         }
+
         let command = (&self.cmd_gen)(ctx);
         let mut cmd = Command::new(&command[0]);
         cmd.args(&command[1..]);
 
-        cmd.stdout(Stdio::piped()).stderr(Stdio::inherit());
+        let enable_stdout = self.stdout_stream.is_some();
+        let enable_stderr = self.stderr_stream.is_some();
+
+        if enable_stdout {
+            cmd.stdout(Stdio::piped());
+        }
+
+        if enable_stderr {
+            cmd.stderr(Stdio::piped());
+        }
 
         let mut child = cmd
             .spawn()
             .map_err(|e| format!("Failed to start subprocess '{}': {}", self.name, e))?;
 
-        self.read_stdout(
-            child.stdout.take(),
-            log_util.stdout_parser,
-            log_util.stdout_sender,
-        );
-        self.read_stderr(
-            child.stderr.take(),
-            log_util.stderr_parser,
-            log_util.stderr_sender,
-        );
+        if let Some((stdout_tx, stdout_parser)) = self.stdout_stream.clone() {
+            if let Some(stdout) = child.stdout.take() {
+                self.read_stdout(Some(stdout), stdout_parser, stdout_tx);
+            }
+        }
+
+        if let Some((stderr_tx, stderr_parser)) = self.stderr_stream.clone() {
+            if let Some(stderr) = child.stderr.take() {
+                self.read_stderr(Some(stderr), stderr_parser, stderr_tx);
+            }
+        }
 
         self.child = Some(child);
         Ok(())
@@ -368,40 +413,26 @@ impl Service for SubProcessService {
         Ok(())
     }
 
-    fn get_log_line_relay(&self) -> LogLineRelay {
-        LogLineRelay {
-            stdout_parser: self.log_relay.stdout_parser.clone(),
-            stderr_parser: self.log_relay.stderr_parser.clone(),
-            stdout_sender: self.log_relay.stdout_sender.clone(),
-            stderr_sender: self.log_relay.stderr_sender.clone(),
-        }
-    }
+    fn add_stdout_parser(&mut self, stdout: StreamParser) { self.stdout_stream = stdout; }
+
+    fn add_stderr_parser(&mut self, stderr: StreamParser) { self.stderr_stream = stderr; }
 }
 
 impl SubProcessService {
     fn read_stdout(
         &mut self,
         stdout: Option<ChildStdout>,
-        output_parser: Option<Arc<dyn Fn(&str) -> Option<String> + Send + Sync + 'static>>,
-        tx: Option<mpsc::Sender<String>>,
+        output_parser: Parser,
+        tx: mpsc::Sender<String>,
     ) {
-        if tx.is_none() {
-            return;
-        }
-        let tx = tx.unwrap();
-
         if let Some(out) = stdout {
             thread::spawn(move || {
                 let reader = BufReader::new(out);
                 for line in reader.lines().flatten() {
-                    if let Some(ref parser) = output_parser {
-                        if let Some(response) = parser(&line) {
-                            if let Err(_) = tx.send(response) {
-                                eprintln!("Failed sending parsed response to channel");
-                            }
+                    if let Some(response) = output_parser(&line) {
+                        if let Err(_) = tx.send(response) {
+                            eprintln!("Failed sending parsed stdout response to channel");
                         }
-                    } else if let Err(_) = tx.send(line) {
-                        eprintln!("Failed sending stdout line to channel");
                     }
                 }
             });
@@ -411,27 +442,17 @@ impl SubProcessService {
     fn read_stderr(
         &mut self,
         stderr: Option<ChildStderr>,
-        output_parser: Option<Arc<dyn Fn(&str) -> Option<String> + Send + Sync + 'static>>,
-        tx: Option<mpsc::Sender<String>>,
+        output_parser: Parser,
+        tx: mpsc::Sender<String>,
     ) {
-        if tx.is_none() {
-            return;
-        }
-        let tx = tx.unwrap();
-
         if let Some(err) = stderr {
             thread::spawn(move || {
                 let reader = BufReader::new(err);
                 for line in reader.lines().flatten() {
-                    println!("[stderr] {}", line);
-                    if let Some(ref parser) = output_parser {
-                        if let Some(response) = parser(&line) {
-                            if let Err(_) = tx.send(response) {
-                                eprintln!("Failed sending parsed stderr response to channel");
-                            }
+                    if let Some(response) = output_parser(&line) {
+                        if let Err(_) = tx.send(response) {
+                            eprintln!("Failed sending parsed stderr response to channel");
                         }
-                    } else if let Err(_) = tx.send(line) {
-                        eprintln!("Failed sending stderr line to channel");
                     }
                 }
             });
@@ -448,35 +469,113 @@ mod tests {
     #[test]
     fn test_start_callapi_stop_python_serve() {
         env_logger::init();
-        let mut harness = TestHarness::new("Anvil", ".");
-        let (stdout_tx, stdout_rx) = mpsc::channel();
-        let (stderr_tx, stderr_rx) = mpsc::channel();
+        let mut harness = TestHarness::new("PythonServerTester", ".");
 
-        harness.add_service(Box::new(SubProcessService {
-            name: "anvil".to_string(),
-            cmd_gen: Box::new(|ctx: ContextArena| {
-                return vec![
-                    "anvil".to_string(),
-                    "-b".to_string(),
-                    "1".to_string(),
-                    "-p".to_string(),
-                    ctx.borrow().get("port").unwrap().to_string(),
-                ];
+        harness.add_service(
+            Box::new(SubProcessService {
+                name: "Python_HTTP_Service".to_string(),
+                cmd_gen: Box::new(|ctx: ContextArena| {
+                    return vec![
+                        "python3".to_string(),
+                        "-m".to_string(),
+                        "http.server".to_string(),
+                        ctx.borrow().get("port").unwrap().to_string(),
+                    ];
+                }),
+                child: None,
+                context_arena: None,
+                stdout_stream: None,
+                stderr_stream: None,
+                description: "Python_HTTP_Service".to_string(),
             }),
-            child: None,
-            context_arena: None,
-            log_relay: LogLineRelay {
-                stdout_parser: Some(Arc::new(|line| {
-                    if line.trim().contains("Block Number: 1") {
-                        return Some("block number 1 found!".to_string());
+            None,
+            None,
+        );
+
+        harness.add_step(TestStep::AsyncFn(Box::new(AsyncFnStep {
+            name: "Call_API".to_string(),
+            description: "Check API response being 200".to_string(),
+            futurefn: Box::new(|ctx| {
+                Box::new(async move {
+                    let random_port: String = "9345".to_string();
+                    ctx.borrow_mut().insert("port".to_string(), random_port);
+                    Ok(())
+                })
+            }),
+        })));
+
+        harness.add_step(TestStep::Service(Box::new(SubProcessServiceStarter {
+            name: "Python_HTTP_Service".to_string(),
+            description: "Starts the Python HTTP server".to_string(),
+            service_idx: 0,
+            wait_after: Some(Duration::from_secs(2)),
+        })));
+
+        harness.add_step(TestStep::AsyncFn(Box::new(AsyncFnStep {
+            name: "Call_API".to_string(),
+            description: "Check API response being 200".to_string(),
+            futurefn: Box::new(|ctx| {
+                Box::new(async move {
+                    let response = reqwest::get(format!(
+                        "http://localhost:{}",
+                        ctx.borrow().get("port").unwrap()
+                    ))
+                    .await;
+
+                    match response {
+                        Ok(resp) =>
+                            if resp.status() == 200 {
+                                Ok(())
+                            } else {
+                                Err(format!("API call failed: Status code {}", resp.status()))
+                            },
+                        Err(e) => Err(format!("Failed to make API call: {}", e)),
                     }
-                    None
-                })),
-                stderr_parser: None,
-                stdout_sender: Some(stdout_tx),
-                stderr_sender: Some(stderr_tx),
-            },
-        }));
+                })
+            }),
+        })));
+
+        harness.add_step(TestStep::Service(Box::new(SubProcessServiceStopper {
+            name: "Python_HTTP_Service".to_string(),
+            description: "Stops the Python HTTP server".to_string(),
+            service_idx: 0,
+            wait_after: None,
+        })));
+
+        harness.execute().expect("Failed to execute test steps");
+    }
+
+    #[test]
+    fn test_anvil_setup() {
+        env_logger::init();
+        let mut harness = TestHarness::new("Anvil", ".");
+
+        let log_stream = harness.add_service(
+            Box::new(SubProcessService {
+                name: "anvil".to_string(),
+                description: "Anvil is a local blockchain node".to_string(),
+                cmd_gen: Box::new(|ctx: ContextArena| {
+                    return vec![
+                        "anvil".to_string(),
+                        "-b".to_string(),
+                        "1".to_string(),
+                        "-p".to_string(),
+                        ctx.borrow().get("port").unwrap().to_string(),
+                    ];
+                }),
+                child: None,
+                context_arena: None,
+                stdout_stream: None,
+                stderr_stream: None,
+            }),
+            Some(Arc::new(|line| {
+                if line.contains("Block Number: 2") {
+                    return Some("chain is running".to_string());
+                }
+                None
+            })),
+            Some(Arc::new(|_line| None)),
+        );
 
         harness.add_step(TestStep::AsyncFn(Box::new(AsyncFnStep {
             name: "ContextArena".to_string(),
@@ -533,17 +632,13 @@ mod tests {
             }),
         })));
 
-        thread::spawn(move || {
-            for line in stdout_rx {
-                println!("[ANVIL STDOUT] {}", line);
-            }
-        });
-
-        thread::spawn(move || {
-            for line in stderr_rx {
-                println!("[ANVIL STDERR] {}", line);
-            }
-        });
+        if let Some(stdout_rx) = log_stream.0 {
+            thread::spawn(move || {
+                for line in stdout_rx {
+                    assert_eq!("chain is running".to_string(), line);
+                }
+            });
+        }
 
         harness.add_step(TestStep::Service(Box::new(SubProcessServiceStopper {
             name: "anvil".to_string(),
