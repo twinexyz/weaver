18957b7a5b07db4c279028aa16ca0a2d0e2970a4
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-06-02 10:39:03 +0545

    feat: ethereum consensus verifier impl

--- crates/precompiles/l1-consensus-verifier/src/chains/mod.rs
+++ crates/precompiles/l1-consensus-verifier/src/chains/mod.rs
@@ -1,9 +1,11 @@
 pub mod ethereum;
 pub mod solana;
 
+use std::fmt::Debug;
+
 use alloy_primitives::Bytes;
 
-pub trait Chains {
+pub trait Chains: Debug {
     fn name(&self) -> String;
-    fn verify(&self, verifying_input: Bytes);
+    fn verify(&self, verifying_input: Bytes) -> Result<(), String>;
 }
