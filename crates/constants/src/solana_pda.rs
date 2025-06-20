4c2cd500525b39ff86264933725c0a656880098a
lilixac <hinewton25@gmail.com>
2025-06-20 13:10:04 +0545

    chore: update pda

--- crates/constants/src/solana_pda.rs
+++ crates/constants/src/solana_pda.rs
@@ -1,3 +1,3 @@
 // PDA Addresses to keep track of
-pub const DEPOSIT_PDA_ADDRESS: &str = "64USSz2BeKC7wAxC3Upi6jhrmowwNviA9A75bvJH5grq";
+pub const DEPOSIT_PDA_ADDRESS: &str = "2CuhxR9Zf2HTtt6JkcJ9ccwVV4ZTYbqFSekgfLxYfvi4";
 pub const WIHTDRAW_PDA_ADDRESS: &str = "8WQ7YGhQRmSfo6yF95RY82TKc888767FfQRsqy2YUf75";
