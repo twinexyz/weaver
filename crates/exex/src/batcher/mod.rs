4b03d7db61c87a6ee0fd21d102eac1ba407ac2c7
lilixac <hinewton25@gmail.com>
2025-07-25 07:41:10 +0545

    fix: update batching logic

--- crates/exex/src/batcher/mod.rs
+++ crates/exex/src/batcher/mod.rs
@@ -1,5 +1,8 @@
-use alloy_consensus::Block;
+use std::marker::PhantomData;
+
+use alloy_consensus::{Block, BlockHeader};
 use alloy_rpc_types::BlockNumHash;
+use eyre::{eyre, Result};
 use futures_util::TryStreamExt;
 use reth_exex::{ExExContext, ExExEvent};
 use reth_node_api::{FullNodeComponents, NodeTypes};
@@ -7,7 +10,7 @@ use reth_primitives::{EthPrimitives, TransactionSigned};
 use reth_provider::{BlockReader, Chain};
 use reth_tracing::tracing::info;
 use twine_db_batch::BatchStore;
-use twine_types::ActiveBatch;
+use twine_types::{ActiveBatch, BlockMetadata};
 
 /// Batch sealing configuration
 #[derive(Debug, Clone)]
@@ -21,70 +24,56 @@ pub struct BatchConfig {
 pub struct TwineBatchingExEx<Node: FullNodeComponents> {
     /// Batch storage
     store: BatchStore,
-    /// Current active batch
-    current_batch: Option<ActiveBatch>,
-    /// Next batch number
-    next_batch_number: u64,
     /// Configuration
     config: BatchConfig,
     /// Execution extension context
     ctx: ExExContext<Node>,
     /// Node context for RPC integration
-    _phantom: std::marker::PhantomData<Node>,
+    _phantom: PhantomData<Node>,
 }
 
 impl<Node> TwineBatchingExEx<Node>
 where
     Node: FullNodeComponents<Types: NodeTypes<Primitives = EthPrimitives>>,
 {
-    /// initialize twine batching exex
-    pub fn new(
-        ctx: ExExContext<Node>,
-        store: BatchStore,
-        config: BatchConfig,
-    ) -> eyre::Result<Self> {
-        let next_batch_number = store.next_batch_number()?;
-        info!("Next batch number is: {}", next_batch_number);
+    /// Initialize twine batching exex
+    pub fn new(ctx: ExExContext<Node>, store: BatchStore, config: BatchConfig) -> Result<Self> {
+        let last_block = store.load_last_height()?.unwrap_or_default();
+        let next_batch_number = store.get_next_batch_number().unwrap_or(0);
+        let chain_tip = ctx.head.number;
 
-        let current_batch = if let Some(mut open) = store.load_open()? {
-            println!("Active Batch is: {:#?}", open);
-            // resume the batch we were building when we crashed
-            open.batch_number = next_batch_number;
-            Some(open)
-        } else {
-            info!("No active batch");
-            // no open batch → we’ll create one on the first block
-            None
-        };
+        info!("Last stored block number: {}", last_block);
+        info!("Next batch number: {}", next_batch_number);
+        info!("Chain tip is: {}", chain_tip);
 
         Ok(Self {
-            next_batch_number,
             store,
-            current_batch,
             config,
             ctx,
-            _phantom: std::marker::PhantomData,
+            _phantom: PhantomData,
         })
     }
 
-    /// start batchmaker execution extension
-    pub async fn start(mut self) -> eyre::Result<()> {
+    /// Start batchmaker execution extension
+    pub async fn start(mut self) -> Result<()> {
         // Process all new chain state notifications
         while let Some(notification) = self.ctx.notifications.try_next().await? {
-            // TODO: Handle revert notifications too maybe
+            // Handle revert notifications
+            if let Some(committed_chain) = notification.reverted_chain() {
+                info!("Reverted chain: {:?}", committed_chain);
+            }
 
             if let Some(committed_chain) = notification.committed_chain() {
-                self.commit(&committed_chain).await?;
-                self.ctx
-                    .events
-                    .send(ExExEvent::FinishedHeight(committed_chain.tip().num_hash()))?;
+                if let Some(finished) = self.commit(&committed_chain).await? {
+                    self.ctx.events.send(ExExEvent::FinishedHeight(finished))?;
+                }
             }
         }
 
         Ok(())
     }
 
-    async fn commit(&mut self, chain: &Chain) -> eyre::Result<Option<BlockNumHash>> {
+    async fn commit(&mut self, chain: &Chain) -> Result<Option<BlockNumHash>> {
         let mut finished_height = None;
         let blocks = chain.blocks();
         let bundles = chain.range().filter_map(|block_number| {
@@ -93,18 +82,14 @@ where
                 .map(|block| block.hash())
                 .zip(chain.execution_outcome_at_block(block_number))
         });
-        let last_processed = self.store.load_last_height()?.unwrap_or(0);
 
         for (block_hash, _) in bundles {
             let current_block = self
                 .ctx
                 .provider()
                 .block_by_hash(block_hash)?
-                .ok_or_else(|| eyre::eyre!("block not found for hash {:?}", block_hash))?;
+                .ok_or_else(|| eyre!("block not found for hash {:?}", block_hash))?;
             let block_index = current_block.number;
-            if block_index <= last_processed {
-                continue;
-            }
             self.process_block(&current_block).await?;
 
             finished_height = Some(BlockNumHash::new(block_index, block_hash));
@@ -113,58 +98,57 @@ where
         Ok(finished_height)
     }
 
-    async fn process_block(&mut self, block: &Block<TransactionSigned>) -> eyre::Result<()> {
-        let batch_number = self.next_batch_number;
-        let batch = self
-            .current_batch
-            .get_or_insert_with(|| ActiveBatch::new(batch_number, block.number));
+    async fn process_block(&mut self, block: &Block<TransactionSigned>) -> Result<()> {
+        let current_block = block.number;
+        let last_block = self.store.load_last_height()?.unwrap_or_default();
 
-        let block_hash = block.hash_slow();
-
-        batch.block_hashes.push(block_hash);
-        batch.state_roots.push(block.state_root);
-
-        // Persist current batch to database
-        self.store.write_open(batch)?;
-
-        // Decide if we have reached the limit.
-        if batch.block_hashes.len() >= self.config.max_blocks as usize {
-            self.seal_batch().await?;
+        // This does not need to run in a loop
+        // If the gap between current_block and last block is high
+        // Then, this current loop seals one batch
+        // Another batch is sealed on next loop
+        if current_block > last_block {
+            if current_block - last_block >= 5 + self.config.max_blocks {
+                let next_batch_start: u64 = last_block + 1;
+                let next_batch_end = next_batch_start + self.config.max_blocks - 1;
+                let next_batch_number = self.store.get_next_batch_number().unwrap_or(0);
+                self.seal_batch(next_batch_number, next_batch_start, next_batch_end)
+                    .await?;
+            }
         }
+
         Ok(())
     }
 
-    async fn seal_batch(&mut self) -> eyre::Result<()> {
-        let batch = self
-            .current_batch
-            .take()
-            .expect("batch exists when sealing");
+    async fn seal_batch(&mut self, next_batch_number: u64, start: u64, end: u64) -> Result<()> {
+        assert_eq!(end - start + 1, self.config.max_blocks);
+        info!(start, end, "Sealing batch {}", next_batch_number);
+        let mut ab = ActiveBatch::new(next_batch_number, start);
 
         let prev_batch_hash = self
             .store
-            .get_batch_hash(self.next_batch_number.saturating_sub(1));
+            .get_batch_hash(next_batch_number.saturating_sub(1));
 
-        let batch_hash = batch.compute_hash(prev_batch_hash);
+        for blk in start..=end {
+            let block = self
+                .ctx
+                .provider()
+                .block_by_number(blk)?
+                .ok_or_else(|| eyre!("block not found for height {:?}", blk))?;
+
+            ab.blocks_metadata.push(BlockMetadata {
+                height: blk,
+                block_hash: block.hash_slow(),
+                state_root: block.state_root(),
+            });
+        }
 
         self.store.seal_batch(
-            self.next_batch_number,
-            batch.start_block..(batch.start_block + batch.block_hashes.len() as u64),
-            batch_hash,
+            next_batch_number,
+            start..end,
+            prev_batch_hash,
+            ab.blocks_metadata,
         )?;
 
-        // After sealing, the current batch is cleared
-        self.store.clear_open()?;
-
-        let finished = BlockNumHash::new(
-            batch.start_block + batch.block_hashes.len() as u64 - 1,
-            *batch.block_hashes.last().unwrap(),
-        );
-        self.ctx.events.send(ExExEvent::FinishedHeight(finished))?;
-
-        // persist it
-        self.store.save_last_height(finished.number)?;
-
-        self.next_batch_number += 1;
         Ok(())
     }
 }
