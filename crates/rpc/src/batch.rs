2b16d5de9d506bbfef5e9d4555ac54f9966e6769
lilixac <hinewton25@gmail.com>
2025-07-25 07:40:05 +0545

    feat: add rpc methods for twine batcher

--- crates/rpc/src/batch.rs
+++ crates/rpc/src/batch.rs
@@ -1,8 +1,10 @@
 use std::ops::Range;
 
 use jsonrpsee::core::RpcResult;
+use jsonrpsee::types::{ErrorCode, ErrorObject};
 use jsonrpsee::RpcModule;
 use twine_db_batch::BatchStore;
+use twine_types::BatchMeta;
 
 use crate::TwineBatchApiServer;
 
@@ -26,11 +28,33 @@ impl TwineBatchApiServer for TwineBatchRPC {
         Ok(self.db.get_batch_hash(batch_number).map(|e| e.to_string()))
     }
 
-    fn get_batch(&self, block_number: u64) -> RpcResult<Option<u64>> {
-        Ok(self.db.find_block_batch(block_number))
+    fn get_batch_number_for_block(&self, block_number: u64) -> RpcResult<Option<u64>> {
+        Ok(self.db.get_batch_number_for_block(block_number))
     }
 
     fn get_blocks_in_batch(&self, batch_number: u64) -> RpcResult<Option<Range<u64>>> {
-        Ok(self.db.get_batch_blocks(batch_number))
+        Ok(self.db.get_blocks_in_batch(batch_number))
+    }
+
+    fn get_latest_batch(&self) -> RpcResult<u64> {
+        match self.db.get_current_batch_number() {
+            Ok(b) => Ok(b),
+            Err(e) => RpcResult::Err(ErrorObject::owned(
+                ErrorCode::InternalError.code(),
+                format!("{:?}", e),
+                Some(0),
+            )),
+        }
+    }
+
+    fn get_full_batch(&self, batch_number: u64) -> RpcResult<BatchMeta> {
+        match self.db.load_batch(batch_number) {
+            Ok(batch) => Ok(batch),
+            Err(e) => RpcResult::Err(ErrorObject::owned(
+                ErrorCode::InternalError.code(),
+                format!("{:?}", e),
+                Some(0),
+            )),
+        }
     }
 }
--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -3,19 +3,28 @@ use std::ops::Range;
 
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::proc_macros::rpc;
+use twine_types::BatchMeta;
 
 /// Twine batch related rpc definition
 pub mod batch;
 
 #[rpc(server, namespace = "twine")]
 pub trait TwineBatchApi {
+    /// Get latest batch
+    #[method(name = "getLatestBatch")]
+    fn get_latest_batch(&self) -> RpcResult<u64>;
+
+    /// Get full batch
+    #[method(name = "getFullBatch")]
+    fn get_full_batch(&self, batch: u64) -> RpcResult<BatchMeta>;
+
     /// Get batch hash for a batch identifier
     #[method(name = "getBatchHash")]
     fn get_batch_hash(&self, batch: u64) -> RpcResult<Option<String>>;
 
     /// Get batch corresponding to a block
-    #[method(name = "getBatch")]
-    fn get_batch(&self, block: u64) -> RpcResult<Option<u64>>;
+    #[method(name = "getBatchNumberForBlock")]
+    fn get_batch_number_for_block(&self, block: u64) -> RpcResult<Option<u64>>;
 
     /// Get all blocks in given batch
     #[method(name = "getBlocksInBatch")]
