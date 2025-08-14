7b1934f2effb98eab86d4f62b1228a4077f410cf
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-08-14 09:58:36 +0545

    feat: transform attempt impl

---
+++ crates/l2-proof-scheduler/src/batch_transform/mod.rs
@@ -0,0 +1,6 @@
+/// bundle consumable by the worker instance to produce
+/// required output
+pub mod transform_attempt;
+/// data consumable by the worker instance to produce  
+/// required output
+pub mod transform_request;
