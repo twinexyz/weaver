437d1530f3d307af9f0b53c8d85c04c7d08b1039
lilixac <hinewton25@gmail.com>
2025-08-20 05:46:28 +0545

    fix: artifacts fetching script

--- scripts/generate_evm_contract_artifacts.sh
+++ scripts/generate_evm_contract_artifacts.sh
@@ -8,7 +8,7 @@ cd twine-solidity-contracts
 ./script/updateSp1Version.sh
 cd -
 
-cat /tmp/twine-solidity-contracts/out/L1MessageQueue.sol/L1MessageQueue.json | jq -r .abi > crates/evm-contracts/res/L1MessageQueue.json
-cat /tmp/twine-solidity-contracts/out/TwineChain.sol/TwineChain.json | jq -r .abi > crates/evm-contracts/res/TwineChain.json
-cat /tmp/twine-solidity-contracts/out/L2TwineMessenger.sol/L2TwineMessenger.json | jq -r .abi > crates/evm-contracts/res/L2TwineMessenger.json
-cat /tmp/twine-solidity-contracts/out/TwineSystemStorageContract.sol/TwineSystemStorageContract.json | jq -r .abi > crates/evm-contracts/res/TwineSystemStorageContract.json
+cat /tmp/twine-solidity-contracts/out/L1MessageHandler.sol/L1MessageHandler.json | jq -r .abi > crates/evm-contracts/artifacts/L1MessageHandler.json
+cat /tmp/twine-solidity-contracts/out/TwineChain.sol/TwineChain.json | jq -r .abi > crates/evm-contracts/artifacts/TwineChain.json
+cat /tmp/twine-solidity-contracts/out/L2TwineMessenger.sol/L2TwineMessenger.json | jq -r .abi > crates/evm-contracts/artifacts/L2TwineMessenger.json
+cat /tmp/twine-solidity-contracts/out/TwineSystemStorage.sol/TwineSystemStorage.json | jq -r .abi > crates/evm-contracts/artifacts/TwineSystemStorage.json
