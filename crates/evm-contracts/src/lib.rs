be647bd34fb8f0001131af091c5053d825433bdb
Newton <57525074+lilixac@users.noreply.github.com>
2025-08-22 15:07:30 +0545

    lilixac: verify state proofs in ethereum txn precompile (#50)
    * feat: transaction precompile for ethereum changes
    
    * feat: updated system storage contract
    
    * feat: update twine bytecode
    
    * feat: updated solidity contract artifacts
    
    * fix: artifacts fetching script
    
    * fix: sync with contract
    
    * feat: added helper structs
    
    * chore: rename struct
    
    * fix: use fixed version of ruzstd
    
    * chore: revert back ruzstd
    
    * fix: zstd precompile should not return error
    
    * chore: error to precompile error
    
    * chore: restructure l1 origin txn public values
    
    * chore: update event signature from current contract branch
    
    * chore: abi update

--- crates/evm-contracts/src/lib.rs
+++ crates/evm-contracts/src/lib.rs
@@ -1,31 +1,118 @@
 //! Wrapper for all EVM Contracts in Twine
 
-use alloy_sol_types::sol;
+pub mod l2_twine_messenger {
+    use alloy_sol_types::sol;
+    use serde::{Deserialize, Serialize};
 
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    TwineChain,
-    "artifacts/TwineChain.json"
-);
+    sol!(
+        #[allow(missing_docs)]
+        #[sol(rpc)]
+        #[derive(Serialize, Deserialize, Debug)]
+        L2TwineMessenger,
+        "artifacts/L2TwineMessenger.json"
+    );
 
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    L2TwineMessenger,
-    "artifacts/L2TwineMessenger.json"
-);
+    sol! {
+        #[derive(Serialize, Deserialize, Debug)]
+        struct TokenTxn {
+            address token;
+            address receiver;
+            bool deposit;
+            uint256 amount;
+        }
 
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    TwineSystemStorageContract,
-    "artifacts/TwineSystemStorageContract.json"
-);
+        #[derive(Serialize, Deserialize, Debug)]
+        struct ContractCall {
+            address targetContract;
+            uint64 value;
+            bytes data;
+        }
 
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    L1MessageQueue,
-    "artifacts/L1MessageQueue.json"
-);
+        #[derive(Serialize, Deserialize, Debug)]
+        struct L1Metadata {
+            uint64 blockHeight;
+            string fromAddress;
+            string l1Token;
+        }
+
+        #[derive(Serialize, Deserialize, Debug)]
+        struct L1Txns {
+            uint64 nonce;
+            TokenTxn tokenTxn;
+            L1Metadata l1Metadata;
+            bytes contractCallData;
+        }
+    }
+}
+
+pub mod twine_chain {
+    use alloy_sol_types::sol;
+    use serde::{Deserialize, Serialize};
+    sol!(
+        #[allow(missing_docs)]
+        #[sol(rpc)]
+        #[derive(Serialize, Deserialize, Debug)]
+        TwineChain,
+        "artifacts/TwineChain.json"
+    );
+
+    sol! {
+        #[derive(Serialize, Deserialize, Debug)]
+        enum TransactionType {
+            Deposit,
+            Withdraw,
+            Message,
+        }
+
+        #[derive(Serialize, Deserialize, Debug)]
+        struct L1OriginTxPublicValues {
+            bytes32 batchHash;
+            uint64 batchNumber;
+            TransactionType txnType;
+            uint64 nonce;
+            uint64 chainId;
+            uint64 blockNumber;
+            bytes32 messageHash;
+            string fromAddress;
+            string toAddress;
+            string l1Token;
+            string l2Token;
+            string amount;
+        }
+
+        #[derive(Serialize, Deserialize, Debug)]
+        struct L2WithdrawPublicValues {
+            uint64 batchNumber;
+            uint64 nonce;
+            bytes32 batchHash;
+            string to;
+            string l1Token;
+            string l2Token;
+            string amount;
+        }
+    }
+}
+
+pub mod twine_system_storage {
+    use alloy_sol_types::sol;
+    use serde::{Deserialize, Serialize};
+    sol!(
+        #[allow(missing_docs)]
+        #[sol(rpc)]
+        #[derive(Serialize, Deserialize, Debug)]
+        TwineSystemStorage,
+        "artifacts/TwineSystemStorage.json"
+    );
+}
+
+pub mod l1_message_handler {
+    use alloy_sol_types::sol;
+    use serde::{Deserialize, Serialize};
+    sol!(
+        #[allow(missing_docs)]
+        #[sol(rpc)]
+        #[derive(Serialize, Deserialize, Debug)]
+        L1MessageHandler,
+        "artifacts/L1MessageHandler.json"
+    );
+}
