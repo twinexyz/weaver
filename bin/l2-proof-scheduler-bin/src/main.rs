4f0e13df974f9a4356cbe26d5cfd22eae850f25a
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-08-29 09:20:15 +0545

    feat: supply config file via cli flags

--- bin/l2-proof-scheduler-bin/src/main.rs
+++ bin/l2-proof-scheduler-bin/src/main.rs
@@ -1,15 +1,21 @@
 //! Twine L2 block execution proof scheduler
 
+use clap::Parser;
 use env_logger;
 use orchestrator_rs::instance::instance::Instance;
 use twine_l2_proof_scheduler::scheduler_instance::instance::TwineProofSchedulerInstance;
 
+/// command line arguments for scheduler binary
+#[derive(Debug, Parser)]
+pub struct Args {
+    /// config file path
+    #[arg(long, short, default_value = "config.toml")]
+    config: String,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
-
-    TwineProofSchedulerInstance::start(format!(
-        "/Users/swopnilparajuli/workspace/work/weaver/config.toml"
-    ))
-    .await;
+    let args = Args::parse();
+    TwineProofSchedulerInstance::start(args.config).await;
 }
