abaf6c907de68129ce69260975f831524f5574ca
Swopnil Parajuli <swopnil@twinelabs.xyz>
2025-09-04 08:34:39 +0545

    Temp/l2 proof scheduler (#59)
    * feat: initiate twine L2 proof scheduler
    
    * wip: checkpoint/emitter implementation with exponential backoff
    
    * chore: documentation
    
    * feat: unified error types for scheduler errors
    
    * feat: transform attempt impl
    
    * feat: attempt creator impl
    
    * cargo: cargo changes
    
    * feat: worker manager impl
    
    * feat: worker manager loop impl
    
    * feat: consumer module impl
    
    * feat: consumer loop impl
    
    * feat: instance impl
    
    * feat: added start scheduler instance
    
    * fix: fixed config value serialization/deserialization
    
    * feat: fetch batch metadata instead of just block range
    
    * fix: client api
    
    * feat: worker instance impl
    
    * feat: worker instance and worker manager integration
    
    * feat: kafka producer impl
    
    * fix: kafka producer implementation
    
    * feat/checkpoint: worker instance integration
    
    * feat(checkpoint): dynamically load start height and transform request id from the database
    
    * feat(checkpoint): reattempt pending transform requests and push result to kafka and postgres
    
    * feat: load correct proofs and logging
    
    * git: gitignore
    
    * fix: integration issues
    
    * fix: failing cargo udeps check
    
    * feat: scheduler <> kafka <> aggregator integration
    
    * feat: supply command line flags to the rsp binary in worker instance
    
    * feat: supply config file via cli flags
    
    * add config for env secret for private repo in docker
    
    * update docker package
    
    * update git creds for private repo pull
    
    * handle pending consume attempts on restarts
    
    * handle pending consume attempts on restarts
    
    * fix: kafka frontend ports
    
    * feat: prune attempts once they are resolved
    
    * feat: backoff and retry to find the starting batch
    
    * feat: keep retrying on failed connection to worker manager
    
    * feat: additional kafka configs
    
    * git: gitignore
    
    * fix: logs
    
    * feat: configurable job completion timeouts
    
    * feat: TA and CA resolve time calculation
    
    * feat: remove path dependency
    
    * chore: documentation
    
    * docker: Dockerfile
    
    * feat: changed ZK proof bundle
    
    * chore: documentation
    
    ---------
    
    Co-authored-by: pen-pal <manish@twinelabs.xyz>

---
+++ crates/l2-proof-scheduler/src/batch_transform/transform_attempt_creator.rs
@@ -0,0 +1,161 @@
+//! creates transform attemtps for all transform requests
+//! recreates transform attempts for failed transform attempts
+
+use std::collections::HashMap;
+use std::time::{self, Duration, Instant};
+
+use async_trait::async_trait;
+use orchestrator_rs::config::Config;
+use orchestrator_rs::transform::{TransformAttempt, TransformAttemptCreator};
+
+use crate::batch_transform::transform_attempt::{
+    TwineBatchTransformAttempt, TwineBatchTransformAttemptID, TwineBatchTransformReturnType,
+};
+use crate::batch_transform::transform_request::{
+    TwineBatchTransformInput, TwineBatchTransformRequest, TwineBatchTransformRequestID,
+};
+use crate::config::TwineProofSchedulerConfig;
+use crate::error::TwineProofSchedulerError;
+
+/// Maximum number of attempts for a request
+pub const DEFAULT_MAX_ATTEMPTS_PER_REQUEST: u64 = 10;
+
+/// Transform attempt creator
+#[derive(Debug, Clone)]
+pub struct TwineBatchTransformAttemptCreator {
+    /// maximum attempts per request, after this is reached, no new reattempts
+    /// will be made
+    max_attempts_per_request: u64,
+    /// stores the latest transform attempts for each requests
+    attempts: HashMap<TwineBatchTransformRequestID, AttemptDetails>, /* TODO: no need to store
+                                                                      * the full attempt */
+}
+
+/// Structure that hold attempts and its creation time
+#[derive(Debug, Clone)]
+pub struct AttemptDetails {
+    /// attempts
+    attempt: TwineBatchTransformAttempt,
+    /// time of creation
+    time: time::Instant,
+}
+
+#[async_trait]
+impl TransformAttemptCreator for TwineBatchTransformAttemptCreator {
+    type Config = TwineProofSchedulerConfig;
+    type Input = TwineBatchTransformInput;
+    type Output = TwineBatchTransformReturnType;
+    type TransformAttempt = TwineBatchTransformAttempt;
+    type TransformAttemptCreationError = TwineProofSchedulerError;
+    type TransformRequest = TwineBatchTransformRequest;
+
+    async fn new(config: std::sync::Arc<tokio::sync::Mutex<Self::Config>>) -> Self
+    where
+        Self: Sized, {
+        let mut max_attempts_per_request = DEFAULT_MAX_ATTEMPTS_PER_REQUEST;
+
+        if let Ok(max_attempts_from_config) = config
+            .lock()
+            .await
+            .get("attempt.max_attempts_per_request".to_string())
+            .await
+        {
+            let max_attempts_from_config: toml::Value =
+                serde_json::from_slice(&max_attempts_from_config)
+                    .expect("could not deserialize config into toml value");
+
+            max_attempts_per_request = max_attempts_from_config.as_integer().unwrap_or(10) as u64;
+        }
+
+        Self {
+            max_attempts_per_request,
+            attempts: HashMap::new(),
+        }
+    }
+
+    /// Converts a `TransformRequest` into a `TransformAttempt`.
+    async fn create_new_attempt(
+        &mut self,
+        transform_attempt_id: Option<<Self::TransformAttempt as TransformAttempt>::Identifier>,
+        request: &Self::TransformRequest,
+    ) -> Result<Self::TransformAttempt, Self::TransformAttemptCreationError> {
+        if let Some(_) = self.attempts.get(&request.identifier) {
+            return Err(TwineProofSchedulerError::KeyAlreadyExists(format!(
+                "{:?}",
+                request.identifier
+            )));
+        }
+        let transform_attempt_id = transform_attempt_id.unwrap_or(TwineBatchTransformAttemptID {
+            identifier: 0,
+            transform_request_id: request.identifier.clone(),
+        });
+        let transform_attempt = TwineBatchTransformAttempt::new(
+            transform_attempt_id,
+            request.call_context.clone(),
+            request.transform_input.clone(),
+        );
+
+        log::info!("new attempt for request {}", request.identifier.identifier);
+
+        let attempt_details = AttemptDetails {
+            attempt: transform_attempt.clone(),
+            time: time::Instant::now(),
+        };
+
+        self.attempts
+            .insert(request.identifier.clone(), attempt_details);
+
+        Ok(transform_attempt)
+    }
+
+    /// Converts a `TransformRequest` into a `TransformAttempt`.
+    async fn create_new_reattempt(
+        &mut self,
+        request: <Self::TransformAttempt as TransformAttempt>::Identifier,
+        error: <Self::TransformAttempt as TransformAttempt>::ReturnPackage,
+    ) -> Result<Self::TransformAttempt, Self::TransformAttemptCreationError> {
+        if let Some(attempt) = self.attempts.get_mut(&request.transform_request_id) {
+            let mut new_identifier = attempt.attempt.identifier.clone();
+            if new_identifier.identifier >= self.max_attempts_per_request {
+                return Err(TwineProofSchedulerError::MaxReattemtsReached(format!(
+                    "{:?}",
+                    request.transform_request_id.clone()
+                )));
+            }
+            new_identifier.identifier += 1;
+            let transform_attempt =
+                TwineBatchTransformAttempt::from_return_package(new_identifier, error);
+
+            let attempt_details = AttemptDetails {
+                attempt: transform_attempt.clone(),
+                time: Instant::now(),
+            };
+
+            *attempt = attempt_details.clone();
+            return Ok(transform_attempt);
+        }
+
+        return Err(TwineProofSchedulerError::KeyNotFound(format!(
+            "{:?}",
+            request.transform_request_id
+        )));
+    }
+
+    async fn prune_attempts(
+        &mut self,
+        attempt_id: <Self::TransformAttempt as TransformAttempt>::Identifier,
+    ) -> Result<Duration, TwineProofSchedulerError> {
+        let transform_request_id: TwineBatchTransformRequestID = attempt_id.into();
+        if let Some(attempts) = self.attempts.remove_entry(&transform_request_id) {
+            log::info!("Removed {:?} from transform attempts record", attempts.0);
+            let elapsed_time = attempts.1.time.elapsed().as_secs();
+            return Ok(Duration::from_secs(elapsed_time));
+        }
+
+        log::warn!(
+            "Key {:?} not found in transform attempts record",
+            transform_request_id
+        );
+        Ok(Duration::from_secs(0))
+    }
+}
---
+++ crates/l2-proof-scheduler/src/worker_manager/connections.rs
@@ -0,0 +1,283 @@
+//! manages connection with the worker managers
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+
+use futures_util::{SinkExt, StreamExt};
+use log::info;
+use orchestrator_rs::worker::worker_manager::WorkerManagerResult;
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpStream;
+use tokio::sync::mpsc::Sender;
+use tokio::sync::Mutex;
+use tokio::time::{self, Instant};
+use tokio_tungstenite;
+
+use crate::batch_transform::transform_attempt::{
+    TwineBatchTransformAttempt, TwineBatchTransformAttemptID, TwineBatchTransformReturnCtx,
+};
+use crate::batch_transform::transform_request::TwineBatchTransformRequestID;
+use crate::error::TwineProofSchedulerError;
+
+/// Connection Message Types
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ConnectionMessageTypes {
+    /// Worker Instances requests new jobs via `NewJob` message type
+    NewJob,
+    /// Result of the job is sent via `JobResult` message type
+    JobResult,
+    /// Invalid params
+    InvalidParams,
+    /// Message not ready
+    MessageNotReady,
+}
+
+/// Message structure sent by the worker instances
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ConnectionMessage {
+    /// Message type to request new job or send result of the
+    /// previously taken job by the worker instances
+    pub message_type: ConnectionMessageTypes,
+    /// Message data
+    pub message: MessageData,
+}
+
+impl ConnectionMessage {
+    /// default connection message with type
+    pub fn default_message_with_type(connection_message_type: ConnectionMessageTypes) -> Self {
+        ConnectionMessage {
+            message_type: connection_message_type,
+            message: MessageData {
+                transform_attempt_id: TwineBatchTransformAttemptID {
+                    identifier: 0,
+                    transform_request_id: TwineBatchTransformRequestID { identifier: 0 },
+                },
+                data: String::new(),
+            },
+        }
+    }
+}
+
+/// Message data
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MessageData {
+    /// Represents the message associated to the Transform Attempt
+    pub transform_attempt_id: TwineBatchTransformAttemptID,
+    /// json serialized message
+    pub data: String,
+}
+
+/// Connection ID
+///
+/// Each connection to the worker instances is uniquely identified by this
+/// connection ID, Worker instances cannot send the job result outside this
+/// connection ID. i.e. If the worker instance reconnects, the job result
+/// of the workers before the disconnect event is not accepted by the
+/// manager
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConnectionID(pub u64);
+
+/// Keeps track of the assigned jobs to the connected worker instances
+#[derive(Debug)]
+pub struct Connections {
+    /// hashmap to store the assigned jobs to the worker instances
+    pub assigned_jobs: Mutex<HashMap<(TwineBatchTransformAttemptID, ConnectionID), JobDetails>>,
+    /// keeps track of the existing connections to the worker instances
+    pub connection_status: Mutex<HashMap<ConnectionID, ConnectionStatus>>,
+    /// job completion timeout
+    pub job_completion_timeout: u64,
+}
+
+/// Connection status of the worker instances
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ConnectionStatus {
+    /// healty connection
+    Healthy,
+    /// disconnected to the workers
+    Disconnected,
+}
+
+/// Structure to represent the details of the assigned jobs to
+/// the worker instances
+#[derive(Debug, Clone)]
+pub struct JobDetails {
+    /// transform attempt
+    pub transform_attempt: TwineBatchTransformAttempt,
+    /// time of assignment
+    /// each job can have a specific timeout period, if the job result
+    /// exceeds this timeout period, the connection to that worker is
+    /// removed and the transform reattempt is made via the processor
+    pub assigned_at: Instant,
+}
+
+impl Connections {
+    /// accepts the incoming connection from the worker instances and handles
+    /// them according to the connection message
+    pub async fn accept_connection(
+        &self,
+        connection_id: ConnectionID,
+        stream: TcpStream,
+        _sender: Sender<WorkerManagerResult<TwineBatchTransformAttempt>>,
+        job_mutex: Arc<Mutex<Option<TwineBatchTransformAttempt>>>,
+    ) {
+        let addr = stream
+            .peer_addr()
+            .expect("connected streams should have a peer address");
+        log::info!("Peer address: {addr}");
+
+        let ws_stream = tokio_tungstenite::accept_async(stream)
+            .await
+            .expect("Error during the websocket handshake occurred");
+
+        log::info!("New WebSocket connection: {addr}");
+        let mut connection_status = self.connection_status.lock().await;
+        connection_status.insert(connection_id.clone(), ConnectionStatus::Healthy);
+        drop(connection_status);
+
+        let (mut write, mut read) = ws_stream.split();
+
+        while let Some(message) = read.next().await {
+            match message {
+                Ok(message) => {
+                    let message = String::from_utf8(message.into_data().to_vec()).unwrap();
+
+                    let connection_message: ConnectionMessage = match serde_json::from_str(&message)
+                    {
+                        Ok(message) => message,
+                        Err(_) => continue,
+                    };
+
+                    match connection_message.message_type {
+                        ConnectionMessageTypes::NewJob => {
+                            if !self.check_connection_status(&connection_id).await {
+                                return;
+                            }
+                            let mut job = job_mutex.lock().await;
+
+                            let message = ConnectionMessage::default_message_with_type(
+                                ConnectionMessageTypes::MessageNotReady,
+                            );
+                            let mut message = serde_json::to_string(&message).unwrap();
+
+                            if let Some(proof_job) = job.clone() {
+                                let id = proof_job.identifier.clone();
+                                let proof_job_str = serde_json::to_string(&proof_job).unwrap();
+                                let job_msg = ConnectionMessage {
+                                    message_type: ConnectionMessageTypes::NewJob,
+                                    message: MessageData {
+                                        transform_attempt_id: id,
+                                        data: proof_job_str,
+                                    },
+                                };
+
+                                message = serde_json::to_string(&job_msg).unwrap();
+                                self.assigned_jobs.lock().await.insert(
+                                    (proof_job.identifier.clone(), connection_id.clone()),
+                                    JobDetails {
+                                        transform_attempt: proof_job.clone(),
+                                        assigned_at: Instant::now(),
+                                    },
+                                );
+                                *job = None;
+                                drop(job);
+                                log::info!(
+                                    "making new job of request id: {:?}",
+                                    proof_job.identifier.transform_request_id
+                                );
+                            }
+
+                            write.send(message.into()).await.unwrap();
+                            write.flush().await.unwrap();
+                            log::info!(
+                                "sent new job to the prover with connection id: {:?}",
+                                connection_id
+                            );
+                        }
+                        ConnectionMessageTypes::JobResult => {
+                            log::info!(
+                                "received job result from prover with connection id: {connection_id:?}",
+                            );
+                            if !self.check_connection_status(&connection_id).await {
+                                return;
+                            }
+
+                            let attempt_id = connection_message.message.transform_attempt_id;
+                            if self
+                                .assigned_jobs
+                                .lock()
+                                .await
+                                .remove(&(attempt_id.clone(), connection_id.clone()))
+                                .is_some()
+                            {
+                                let worker_manager_result: WorkerManagerResult<
+                                    TwineBatchTransformAttempt,
+                                > = serde_json::from_str(&connection_message.message.data).unwrap(); // TODO: write some message
+                                info!(
+                                    "received job result for job with request id: {:?}",
+                                    attempt_id.transform_request_id
+                                );
+                                _sender.send(worker_manager_result).await.unwrap();
+                            } else {
+                                log::error!(
+                                    "ignored job result received from different connection boundry"
+                                );
+                            }
+                        }
+                        ConnectionMessageTypes::InvalidParams
+                        | ConnectionMessageTypes::MessageNotReady => {} /* todo: terminate
+                                                                         * connection */
+                    }
+                }
+                Err(e) => log::error!("{e}"),
+            }
+        }
+    }
+
+    async fn check_connection_status(&self, connection_id: &ConnectionID) -> bool {
+        if let Some(connection_status) = self.connection_status.lock().await.get(connection_id) {
+            if connection_status.clone() == ConnectionStatus::Disconnected {
+                log::warn!("connection status: Disconnected");
+                return false;
+            }
+            return true;
+        }
+        false
+    }
+
+    /// manages job assignments timeouts
+    pub async fn handle_assigned_jobs(
+        &self,
+        _sender: Sender<WorkerManagerResult<TwineBatchTransformAttempt>>,
+    ) {
+        let mut interval = time::interval(Duration::from_secs(self.job_completion_timeout));
+        loop {
+            interval.tick().await;
+            let mut assigned_jobs = self.assigned_jobs.lock().await;
+            let mut connection_status = self.connection_status.lock().await;
+            let jobs = assigned_jobs.clone();
+            for (job_id, job_details) in jobs {
+                // check if the job has timed out
+                // losen the policy
+                if job_details.assigned_at.elapsed().as_secs() > self.job_completion_timeout {
+                    assigned_jobs.remove(&job_id.clone()).unwrap();
+                    connection_status.insert(job_id.1.clone(), ConnectionStatus::Disconnected);
+                    // let mut return_package = job_details.transform_attempt.return_package;
+
+                    let return_package = (
+                        job_id.0.clone(),
+                        TwineBatchTransformReturnCtx {
+                            call_context: job_details.transform_attempt.call_ctx,
+                            call_type: job_details.transform_attempt.call_val,
+                            extra_data: vec![],
+                        },
+                        Err(TwineProofSchedulerError::Other("job timed out".to_string())),
+                    );
+
+                    let result: WorkerManagerResult<TwineBatchTransformAttempt> =
+                        WorkerManagerResult::Failure(job_id.0, return_package);
+                    _sender.send(result).await.unwrap();
+                }
+            }
+        }
+    }
+}
