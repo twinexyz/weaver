27346ca60a756bf23795d01d1df95e7f025b961a
lilixac <hinewton25@gmail.com>
2025-09-12 19:18:10 +0545

    chore: default message queue address

--- crates/constants/src/eth_contracts.rs
+++ crates/constants/src/eth_contracts.rs
@@ -3,7 +3,7 @@ use alloy_primitives::{address, Address};
 pub const ETHEREUM_MESSAGE_QUEUE: Address = address!("610178dA211FEF7D417bC0e6FeD39F05609AD788");
 
 pub const ETHEREUM_SEPOLIA_MESSAGE_QUEUE: Address =
-    address!("610178dA211FEF7D417bC0e6FeD39F05609AD788");
+    address!("4aDAfB6072777f0Cf880eFc8f9f445B78E92d9b9");
 
 pub const ETHEREUM_HOLESKY_MESSAGE_QUEUE: Address =
     address!("f0bA90cF1fF8d81a3f3E539CF503edF937dfC46C");
