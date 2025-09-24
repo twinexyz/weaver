30e0493f9b764000aedd7fab86e1236701f9f4ef
nobel <nobel2073@gmail.com>
2025-09-24 23:45:01 +0545

    chore: remove unwanted files

--- crates/integration-tests/src/solidity_contracts/mod.rs
+++ crates/integration-tests/src/solidity_contracts/mod.rs
@@ -67,7 +67,7 @@ pub fn build_contracts_step(contract_path: &Path) -> eyre::Result<TestStep> {
                 let status = Command::new("forge")
                     .arg("build")
                     .current_dir(&path)
-                    .stdout(Stdio::null())
+                    .stdout(Stdio::inherit())
                     .stderr(Stdio::inherit())
                     .status()?;
 
@@ -91,7 +91,7 @@ pub fn deploy_contracts_step(contract_path: &Path) -> eyre::Result<TestStep> {
                 let status = Command::new("bash")
                     .args(["-lc", "./script/configure.sh --clear"])
                     .current_dir(&path)
-                    .stdout(Stdio::null())
+                    .stdout(Stdio::inherit())
                     .stderr(Stdio::inherit())
                     .status()?;
 
