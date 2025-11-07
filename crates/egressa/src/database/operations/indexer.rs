e2115467ee0585e1313d196da74a1060c8f2c8a7
Rabindra Regmi <regmirabindra2014@gmail.com>
2025-11-07 14:20:06 +0545

    add: max block height filter included in db query (#107)

--- crates/egressa/src/database/operations/indexer.rs
+++ crates/egressa/src/database/operations/indexer.rs
@@ -28,76 +28,156 @@ impl<'a> IndexerOperations<'a> {
     ///   handle_block_number is always null)
     /// - For Deposit and ForcedWithdraw events: sort_height =
     ///   handle_block_number (l2_handle_height)
-    pub async fn find_pending_transaction_events(&self) -> Result<Vec<FetchedIndexerEvent>> {
-        let events = sqlx::query_as::<_, FetchedIndexerEvent>(
-        r#"
-        WITH pending_events AS (
-            -- Deposit events: has l2_handle_tx_hash, status 0, no l1_execute_hash, is_completed = false
-            SELECT
-                st.chain_id as l1_chain_id,
-                st.nonce,
-                st.transaction_type::text,
-                tf.handle_block_number AS l2_block_height,
-                st.l1_token,
-                st.l2_token,
-                st.l1_address,
-                st.transaction_hash as source_transaction_hash,
-                tf.handle_tx_hash as l2_transaction_hash,
-                tf.handle_status
-            FROM source_transactions st
-            JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
-            WHERE st.transaction_type = 'Deposit'::transaction_type_enum
-              AND tf.handle_tx_hash IS NOT NULL
-              AND tf.handle_status = 2
-              AND tf.execute_tx_hash IS NULL
-              AND tf.is_completed = false
+    ///
+    /// If max_block_height is provided, only events with block height <=
+    /// max_block_height are returned
+    pub async fn find_pending_transaction_events(
+        &self,
+        max_block_height: Option<u64>,
+    ) -> Result<Vec<FetchedIndexerEvent>> {
+        let query = if let Some(max_height) = max_block_height {
+            sqlx::query_as::<_, FetchedIndexerEvent>(
+                r#"
+                WITH pending_events AS (
+                    -- Deposit events: has l2_handle_tx_hash, status 0, no l1_execute_hash, is_completed = false
+                    SELECT
+                        st.chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        tf.handle_block_number AS l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        tf.handle_tx_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'Deposit'::transaction_type_enum
+                      AND tf.handle_tx_hash IS NOT NULL
+                      AND tf.handle_status = 0
+                      AND tf.execute_tx_hash IS NULL
+                      AND tf.is_completed = false
+                      AND tf.handle_block_number <= $1
 
-            UNION ALL
+                    UNION ALL
 
-            -- Withdraw events: only those without corresponding transaction_flow records
-            SELECT
-                st.destination_chain_id as l1_chain_id,
-                st.nonce,
-                st.transaction_type::text,
-                st.block_number as l2_block_height,
-                st.l1_token,
-                st.l2_token,
-                st.l1_address,
-                st.transaction_hash as source_transaction_hash,
-                st.transaction_hash as l2_transaction_hash,
-                tf.handle_status
-            FROM source_transactions st
-            LEFT JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
-            WHERE st.transaction_type = 'Withdraw'::transaction_type_enum
-              AND tf.chain_id IS NULL
+                    -- Withdraw events: only those without corresponding transaction_flow records
+                    SELECT
+                        st.destination_chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        st.block_number as l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        st.transaction_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    LEFT JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'Withdraw'::transaction_type_enum
+                      AND tf.chain_id IS NULL
+                      AND st.block_number <= $1
 
-            UNION ALL
+                    UNION ALL
 
-            -- Forced withdraw events: has l2_handle_hash but no l1_execute_hash
-            SELECT
-                st.chain_id as l1_chain_id,
-                st.nonce,
-                st.transaction_type::text,
-                tf.handle_block_number as l2_block_height,
-                st.l1_token,
-                st.l2_token,
-                st.l1_address,
-                st.transaction_hash as source_transaction_hash,
-                tf.handle_tx_hash as l2_transaction_hash,
-                tf.handle_status
-            FROM source_transactions st
-            JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
-            WHERE st.transaction_type = 'ForcedWithdraw'::transaction_type_enum
-              AND tf.handle_tx_hash IS NOT NULL
-              AND tf.execute_tx_hash IS NULL
-              AND tf.handle_status = 1
-        )
-        SELECT * FROM pending_events
-        ORDER BY l2_block_height ASC NULLS LAST
-        "#
-    )
-    .fetch_all(self.db_pool)
-    .await?;
+                    -- Forced withdraw events: has l2_handle_hash but no l1_execute_hash
+                    SELECT
+                        st.chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        tf.handle_block_number as l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        tf.handle_tx_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'ForcedWithdraw'::transaction_type_enum
+                      AND tf.handle_tx_hash IS NOT NULL
+                      AND tf.execute_tx_hash IS NULL
+                      AND tf.handle_status = 1
+                      AND tf.handle_block_number <= $1
+                )
+                SELECT * FROM pending_events
+                ORDER BY l2_block_height ASC NULLS LAST
+                "#
+            )
+            .bind(max_height as i64)
+        } else {
+            sqlx::query_as::<_, FetchedIndexerEvent>(
+                r#"
+                WITH pending_events AS (
+                    -- Deposit events: has l2_handle_tx_hash, status 0, no l1_execute_hash, is_completed = false
+                    SELECT
+                        st.chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        tf.handle_block_number AS l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        tf.handle_tx_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'Deposit'::transaction_type_enum
+                      AND tf.handle_tx_hash IS NOT NULL
+                      AND tf.handle_status = 0
+                      AND tf.execute_tx_hash IS NULL
+                      AND tf.is_completed = false
+
+                    UNION ALL
+
+                    -- Withdraw events: only those without corresponding transaction_flow records
+                    SELECT
+                        st.destination_chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        st.block_number as l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        st.transaction_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    LEFT JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'Withdraw'::transaction_type_enum
+                      AND tf.chain_id IS NULL
+
+                    UNION ALL
+
+                    -- Forced withdraw events: has l2_handle_hash but no l1_execute_hash
+                    SELECT
+                        st.chain_id as l1_chain_id,
+                        st.nonce,
+                        st.transaction_type::text,
+                        tf.handle_block_number as l2_block_height,
+                        st.l1_token,
+                        st.l2_token,
+                        st.l1_address,
+                        st.transaction_hash as source_transaction_hash,
+                        tf.handle_tx_hash as l2_transaction_hash,
+                        tf.handle_status
+                    FROM source_transactions st
+                    JOIN transaction_flows tf ON st.chain_id = tf.chain_id AND st.nonce = tf.nonce
+                    WHERE st.transaction_type = 'ForcedWithdraw'::transaction_type_enum
+                      AND tf.handle_tx_hash IS NOT NULL
+                      AND tf.execute_tx_hash IS NULL
+                      AND tf.handle_status = 1
+                )
+                SELECT * FROM pending_events
+                ORDER BY l2_block_height ASC NULLS LAST
+                "#,
+            )
+        };
+
+        let events = query.fetch_all(self.db_pool).await?;
 
         Ok(events)
     }
--- crates/egressa/src/polling.rs
+++ crates/egressa/src/polling.rs
@@ -1,6 +1,7 @@
 use eyre::Result;
-use reth_tracing::tracing::{info, warn};
+use reth_tracing::tracing::{error, info, warn};
 
+use crate::chains::twine::provider::TwineProvider;
 use crate::database::client::DbClient;
 use crate::types::{WithdrawalEvent, WithdrawalEventType};
 
@@ -10,13 +11,30 @@ pub struct WithdrawalEventPoller;
 
 impl WithdrawalEventPoller {
     /// Poll for pending transaction events from indexer database
-    pub async fn poll_events(&self, db_client: DbClient) -> Result<Vec<WithdrawalEvent>> {
+    pub async fn poll_events(
+        &self,
+        db_client: DbClient,
+        twine_provider: &TwineProvider,
+    ) -> Result<Vec<WithdrawalEvent>> {
         info!("Polling for pending transaction events from database...");
 
-        // Query the database for pending events
+        // Get the latest batch information to filter events
+        let max_block_height = match Self::get_latest_batched_block_height(twine_provider).await {
+            Ok(height) => {
+                info!("Latest batched block height: {}", height);
+                Some(height)
+            }
+            Err(e) => {
+                error!("Failed to get latest batched block height: {}", e);
+                warn!("Proceeding without block height filter - this may cause processing of unbatched blocks");
+                None
+            }
+        };
+
+        // Query the database for pending events with block height filter
         let events = db_client
             .indexer()
-            .find_pending_transaction_events()
+            .find_pending_transaction_events(max_block_height)
             .await
             .map_err(|e| {
                 warn!("Failed to query pending transaction events: {}", e);
@@ -60,5 +78,30 @@ impl WithdrawalEventPoller {
 
         info!("Converted to {} withdrawal events", withdrawal_events.len());
         Ok(withdrawal_events)
+    }
+
+    /// Get the latest block height that is included in a batch
+    async fn get_latest_batched_block_height(twine_provider: &TwineProvider) -> Result<u64> {
+        // Get the latest batch number
+        let latest_batch_number = twine_provider.batch_client.get_latest_batch().await?;
+
+        // Get the latest batch details to understand its block range
+        let latest_batch = twine_provider
+            .batch_client
+            .get_full_batch(latest_batch_number, Some(false))
+            .await?;
+
+        let block_range = latest_batch.block_range();
+        let latest_block_in_batch = *block_range.end();
+
+        info!(
+            "Latest batch {} contains blocks {}..{}, using {} as max block height",
+            latest_batch_number,
+            block_range.start(),
+            latest_block_in_batch,
+            latest_block_in_batch
+        );
+
+        Ok(latest_block_in_batch)
     }
 }
